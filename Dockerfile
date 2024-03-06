444e5798a2efdb80d29531498387ec8807bf60af
kmou424 <me@kmou424.moe>
2024-03-07 00:42:50 +0800

    chore(deploy): introduce Dockerfile and docker-compose.yml

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM golang:latest AS builder
+
+WORKDIR /app
+
+ENV GOPROXY https://goproxy.cn,direct
+
+COPY . /app
+
+RUN go mod download
+RUN go build -v=0 -a -trimpath -ldflags "-s -w -extldflags '-static'" -o server_bin ./cmd/server
+
+FROM scratch
+WORKDIR /app
+COPY --from=builder /app/server_bin /app/server
+CMD ["/app/server"]
---
+++ deploy/deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+label() {
+  echo "===== $1 ====="
+}
+
+print() {
+  echo "==> $1"
+}
+
+function clone() {
+    local repo_url=$1
+    local folder_name=$2
+
+    if [ -d "$folder_name" ]; then
+        cd "$folder_name" || return 1
+        if git pull; then
+            print "pull successful"
+        else
+            print "pull failed, try re-cloning"
+            cd ..
+            rm -rf "$folder_name"
+            git clone "$repo_url" "$folder_name"
+        fi
+    else
+        git clone "$repo_url" "$folder_name" || return 1
+    fi
+}
+
+cd "$(dirname "$0")" || exit
+
+label "clone resonance-api"
+clone git@github.com:kmou424/resonance-api.git resonance-api || exit 1
+
+label "clone resonance-dataserver"
+clone git@github.com:kmou424/resonance-dataserver.git resonance-dataserver || exit 1
+
+label "deploying apps..."
+docker compose -f ./docker-compose.yml up -d
