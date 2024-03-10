e1a847e89c3bb48f7949abda0b888e31ec593e66
kmou424 <me@kmou424.moe>
2024-03-10 20:30:43 +0800

    chore(rediskit): better logging

--- internal/kits/rediskit/get.go
+++ internal/kits/rediskit/get.go
@@ -1,16 +1,22 @@
 package rediskit
 
 import (
+	"errors"
 	"fmt"
 	"github.com/charmbracelet/log"
 	"github.com/gookit/goutil/mathutil"
 	"github.com/kmou424/resonance-dataserver/cache"
+	"github.com/redis/go-redis/v9"
 )
 
 func GetValueFromRedis(key string, def any) any {
 	val, err := cache.Get(key)
+	if errors.Is(err, redis.Nil) {
+		log.Warn(fmt.Sprintf("get value [%s] failed: not exists", key))
+		return def
+	}
 	if err != nil {
-		log.Warn(fmt.Sprintf("get entry of good info failed: [%s]", key))
+		log.Warn(fmt.Sprintf("get value [%s] failed", key))
 		return def
 	}
 	return val
